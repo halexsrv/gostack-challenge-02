0de65b9d7c9b78b837fdc01f44707f9d125fd0e5
Alex Vitorino <halexsrv@gmail.com>
2020-02-04 22:56:42 -0300

    add authenticate route

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,13 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
